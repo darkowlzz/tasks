110489a3fcae53220407b60181768d2582ac0e6a
decipher <rodneyd.teal@gmail.com>
2014-03-01 05:46:18 -0600

    Updated for AMD

--- data/sidebar.js
+++ data/sidebar.js
@@ -1,81 +1,84 @@
-// Collect and create Task items.
-addon.port.on("list", (list) => {
-  list.forEach((task) => Item(task));
-});
+require(['scripts/jquery-2.1.0.js'], function () {
 
-// Create a task item.
-function Item(name) {
-  var taskItem = document.createElement("div");
-  taskItem.className = "taskItem";
+    // Collect and create Task items.
+    addon.port.on("list", (list) => {
+      list.forEach((task) => Item(task));
+    });
 
-  var title = document.createElement("span");
-  title.className = "title";
-  title.textContent = name;
-  taskItem.appendChild(title);
+    // Create a task item.
+    function Item(name) {
+      var taskItem = document.createElement("div");
+      taskItem.className = "taskItem";
 
-  var del = document.createElement("span");
-  del.className = "del";
-  del.textContent = "x";
-  taskItem.appendChild(del);
+      var title = document.createElement("span");
+      title.className = "title";
+      title.textContent = name;
+      taskItem.appendChild(title);
 
-  del.onclick = function() { 
-    addon.port.emit("pop", name);
-    taskItem.remove();
-  }
+      var del = document.createElement("span");
+      del.className = "del";
+      del.textContent = "x";
+      taskItem.appendChild(del);
 
-  $("#task-container").append(taskItem);
-}
+      del.onclick = function() {
+        addon.port.emit("pop", name);
+        taskItem.remove();
+      }
 
-// Push task into the container.
-$("#push-task").click(function(){
-  var taskName = $("#new-task");
-  
-  if (taskName.val() != "") {
-    Item(taskName.val());
-    // Push task in simple-storage.
-    addon.port.emit("push", taskName.val());
-   
-    $("#new-task").val("");
-  }
-  // Scroll to bottom.
-  $("html, body").animate({ scrollTop: $(document).height() }, 300);
-});
-
-// Submit task when pressed enter.
-$("#new-task").keyup(function(event) {
-  if (event.keyCode == 13) {
-    $("#push-task").click();
-  }
-});
-
-// Task pop
-$("#pop-task").click(function(){
-  var fRadio = $("#fifo-radio");
-  var lRadio = $("#lifo-radio");
-  // FIFO pop
-  if (fRadio.is(":checked")) {
-    if ($("#task-container").children().length > 0) {
-      var removeTask = $("#task-container").children()[0];
-      // Remove from simple-storage.
-      addon.port.emit("pop", removeTask.firstChild.textContent)
-      removeTask.remove();
-      // Scroll up.
-      $("html, body").animate({ scrollTop: 0 }, 300);
+      $("#task-container").append(taskItem);
     }
-  }
-  // LIFO pop
-  else if (lRadio.is(":checked")) {
-    if ($("#task-container").children().length > 0) {
-      var container = $("#task-container");
-      var length = container.children().length;
-      var removeTask = container.children()[length-1];
-      addon.port.emit("pop", removeTask.firstChild.textContent);
-      removeTask.remove();
-      // Scroll down.
+
+    // Push task into the container.
+    $("#push-task").click(function(){
+      var taskName = $("#new-task");
+
+      if (taskName.val() != "") {
+        Item(taskName.val());
+        // Push task in simple-storage.
+        addon.port.emit("push", taskName.val());
+
+        $("#new-task").val("");
+      }
+      // Scroll to bottom.
       $("html, body").animate({ scrollTop: $(document).height() }, 300);
-    }
-  }
-});
+    });
 
-// Emit `ready` to let add-on script know that we are ready!
-addon.port.emit("ready");
+    // Submit task when pressed enter.
+    $("#new-task").keyup(function(event) {
+      if (event.keyCode == 13) {
+        $("#push-task").click();
+      }
+    });
+
+    // Task pop
+    $("#pop-task").click(function(){
+      var fRadio = $("#fifo-radio");
+      var lRadio = $("#lifo-radio");
+      // FIFO pop
+      if (fRadio.is(":checked")) {
+        if ($("#task-container").children().length > 0) {
+          var removeTask = $("#task-container").children()[0];
+          // Remove from simple-storage.
+          addon.port.emit("pop", removeTask.firstChild.textContent)
+          removeTask.remove();
+          // Scroll up.
+          $("html, body").animate({ scrollTop: 0 }, 300);
+        }
+      }
+      // LIFO pop
+      else if (lRadio.is(":checked")) {
+        if ($("#task-container").children().length > 0) {
+          var container = $("#task-container");
+          var length = container.children().length;
+          var removeTask = container.children()[length-1];
+          addon.port.emit("pop", removeTask.firstChild.textContent);
+          removeTask.remove();
+          // Scroll down.
+          $("html, body").animate({ scrollTop: $(document).height() }, 300);
+        }
+      }
+    });
+
+    // Emit `ready` to let add-on script know that we are ready!
+    addon.port.emit("ready");
+});
